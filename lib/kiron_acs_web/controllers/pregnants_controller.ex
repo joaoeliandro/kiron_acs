a58e34a52e25d2d4e6a217e184a8a7ba6f085b2b
joaoeliandro <eliandrogermano@gmail.com>
2021-06-15 14:18:47 -0300

    Adicionado a rota de listagem de todos pregnants

--- lib/kiron_acs_web/controllers/pregnants_controller.ex
+++ lib/kiron_acs_web/controllers/pregnants_controller.ex
@@ -15,8 +15,20 @@ defmodule KironAcsWeb.PregnantsController do
     end
   end
 
+  def show(conn, _params) do
+    task = Task.async(fn -> KironAcs.get_all_pregnants() end)
+
+    with {:ok, pregnants} <- Task.await(task) do
+      conn
+      |> put_status(:ok)
+      |> render("list_pregnants.json", pregnants: pregnants)
+    end
+  end
+
   def get_all_by_agent_id(conn, %{"agent_id" => agent_id}) do
-    with pregnants <- KironAcs.get_pregnants_by_agent_id(agent_id) do
+    task = Task.async(fn -> KironAcs.get_pregnants_by_agent_id(agent_id) end)
+
+    with {:ok, pregnants} <- Task.await(task) do
       conn
       |> put_status(:ok)
       |> render("list_pregnants_by_agent_id.json", pregnants: pregnants)
