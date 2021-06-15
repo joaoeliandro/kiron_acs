62d6fca61312cff0ee66d03d4877dbd5783d393d
joaoeliandro <eliandrogermano@gmail.com>
2021-06-15 16:03:54 -0300

    Adicionado o retorno do token no json

--- lib/kiron_acs_web/views/agents_view.ex
+++ lib/kiron_acs_web/views/agents_view.ex
@@ -1,7 +1,12 @@
 defmodule KironAcsWeb.AgentsView do
   use KironAcsWeb, :view
 
-  def render("create.json", %{agent: agent}) do
-    agent
+  alias KironAcs.Agent
+
+  def render("create.json", %{token: token, agent: %Agent{} = agent}) do
+    %{
+      agent: agent,
+      token: token
+    }
   end
 end
