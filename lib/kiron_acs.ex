b80362e2ab34fc312f7bd6bf56e0ba805d421ba0
joaoeliandro <eliandrogermano@gmail.com>
2021-06-14 22:23:07 -0300

    Adicionado os modulos delegados do pregnants

--- lib/kiron_acs.ex
+++ lib/kiron_acs.ex
@@ -1,5 +1,11 @@
 defmodule KironAcs do
   alias KironAcs.Agents.Create, as: AgentCreate
 
+  alias KironAcs.Pregnants.Create, as: PregnantsCreate
+  alias KironAcs.Pregnants.Get, as: PregnantsGet
+
   defdelegate create_agent(params), to: AgentCreate, as: :call
+
+  defdelegate create_pregnant(params), to: PregnantsCreate, as: :call
+  defdelegate get_pregnants_by_agent_id(agent_id), to: PregnantsGet, as: :get_all_by_agent_id
 end
