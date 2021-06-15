24e71d0ed5ba18654d327422273b1d599ed6ca7f
joaoeliandro <eliandrogermano@gmail.com>
2021-06-15 16:02:55 -0300

    Adicionado o retorno do token na criação do agent no controller

--- lib/kiron_acs_web/controllers/agents_controller.ex
+++ lib/kiron_acs_web/controllers/agents_controller.ex
@@ -3,15 +3,16 @@ defmodule KironAcsWeb.AgentsController do
 
   alias KironAcs.Agent
 
-  alias KironAcsWeb.FallbackController
+  alias KironAcsWeb.{Auth.Guardian, FallbackController}
 
   action_fallback FallbackController
 
   def create(conn, params) do
-    with {:ok, %Agent{} = agent} <- KironAcs.create_agent(params) do
+    with {:ok, %Agent{} = agent} <- KironAcs.create_agent(params),
+         {:ok, token, _claims} <- Guardian.encode_and_sign(agent) do
       conn
       |> put_status(:created)
-      |> render("create.json", agent: agent)
+      |> render("create.json", token: token, agent: agent)
     end
   end
 end
