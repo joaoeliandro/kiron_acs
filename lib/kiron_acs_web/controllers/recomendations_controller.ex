988009caa80c21f82615a89d530d4ffb04aaace2
joaoeliandro <eliandrogermano@gmail.com>
2021-06-27 19:02:00 -0300

    Adicionado os controllers de get de recomendations

--- lib/kiron_acs_web/controllers/recomendations_controller.ex
+++ lib/kiron_acs_web/controllers/recomendations_controller.ex
@@ -14,4 +14,34 @@ defmodule KironAcsWeb.RecomendationsController do
       |> render("create.json", recomendation: recomendation)
     end
   end
+
+  def show(conn, _params) do
+    task = Task.async(fn -> KironAcs.get_all_recomendations() end)
+
+    with {:ok, recomendations} <- Task.await(task) do
+      conn
+      |> put_status(:ok)
+      |> render("list_recomendations.json", recomendations: recomendations)
+    end
+  end
+
+  def get_all_by_agent_id(conn, %{"agent_id" => agent_id}) do
+    task = Task.async(fn -> KironAcs.get_recomendations_by_agent_id(agent_id) end)
+
+    with {:ok, recomendations} <- Task.await(task) do
+      conn
+      |> put_status(:ok)
+      |> render("list_recomendations_by_agent_id.json", recomendations: recomendations)
+    end
+  end
+
+  def get_all_by_pregnant_id(conn, %{"pregnant_id" => pregnant_id}) do
+    task = Task.async(fn -> KironAcs.get_recomendations_by_pregnant_id(pregnant_id) end)
+
+    with {:ok, recomendations} <- Task.await(task) do
+      conn
+      |> put_status(:ok)
+      |> render("list_recomendations_by_pregnant_id.json", recomendations: recomendations)
+    end
+  end
 end
