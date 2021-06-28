e01019bc9f1f6fbda8d8c90d00a25698bf2a7817
joaoeliandro <eliandrogermano@gmail.com>
2021-06-28 01:52:39 -0300

    Alterado forma de renderização das recomendações

--- lib/kiron_acs_web/views/recomendations_view.ex
+++ lib/kiron_acs_web/views/recomendations_view.ex
@@ -4,19 +4,17 @@ defmodule KironAcsWeb.RecomendationsView do
   alias KironAcs.Recomendation
 
   def render("create.json", %{recomendation: %Recomendation{} = recomendation}) do
-    recomendation
+    render_one(recomendation, __MODULE__, "recomendation.json")
   end
 
   def render("list_recomendations.json", %{
-        recomendations: [%Recomendation{} | _tail] = recomendations
+        recomendations: recomendations
       }) do
-    %{
-      recomendations: recomendations
-    }
+    render_many(recomendations, KironAcsWeb.RecomendationsView, "recomendation.json")
   end
 
   def render("list_recomendations_by_agent_id.json", %{
-        recomendations: [%Recomendation{} | _tail] = recomendations
+        recomendations: recomendations
       }) do
     %{
       recomendations: recomendations
@@ -24,10 +22,14 @@ defmodule KironAcsWeb.RecomendationsView do
   end
 
   def render("list_recomendations_by_pregnant_id.json", %{
-        recomendations: [%Recomendation{} | _tail] = recomendations
+        recomendations: recomendations
       }) do
     %{
       recomendations: recomendations
     }
+  end
+
+  def render("recomendation.json", %{recomendations: %Recomendation{} = recomendation}) do
+    recomendation
   end
 end
