5f5cb1b0f0a01c4535ef0d6866fa2e32e01eb9e6
joaoeliandro <eliandrogermano@gmail.com>
2021-06-28 02:15:05 -0300

    Corrigindo bug de releases e pool_size

--- config/releases.exs
+++ config/releases.exs
@@ -10,7 +10,7 @@ database_url =
 config :kiron_acs, KironAcs.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "4")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
