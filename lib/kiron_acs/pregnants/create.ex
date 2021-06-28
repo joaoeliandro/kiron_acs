8b1e4b0f8d294af1a552dd6e9d0c99cabfbc270f
joaoeliandro <eliandrogermano@gmail.com>
2021-06-28 02:05:03 -0300

    Corrigido bug de preload na criação de pregnants

--- lib/kiron_acs/pregnants/create.ex
+++ lib/kiron_acs/pregnants/create.ex
@@ -1,10 +1,23 @@
 defmodule KironAcs.Pregnants.Create do
   alias KironAcs.{Pregnant, Repo}
 
+  alias Ecto.Multi
+
   def call(params) do
+    Multi.new()
+    |> Multi.run(:insert_pregnant, fn repo, _changes ->
+      insert_pregnant(repo, params)
+    end)
+    |> Multi.run(:preload_data, fn repo, %{insert_pregnant: pregnant} ->
+      preload_data(repo, pregnant)
+    end)
+    |> run_transaction()
+  end
+
+  defp insert_pregnant(repo, params) do
     params
     |> Pregnant.changeset()
-    |> Repo.insert()
+    |> repo.insert()
     |> handle_insert()
   end
 
@@ -12,5 +25,19 @@ defmodule KironAcs.Pregnants.Create do
 
   defp handle_insert({:error, result}) do
     {:error, %{result: result, status: :bad_request}}
+  end
+
+  defp preload_data(repo, pregnant) do
+    {:ok, repo.preload(pregnant, [:agent])}
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} ->
+        {:error, reason}
+
+      {:ok, %{preload_data: pregnant}} ->
+        {:ok, pregnant}
+    end
   end
 end
