3d4ebb4a51bc939fa13fa26a4756feab7130dea4
João Eliandro Germano <eliandrogermano@gmail.com>
2021-06-29 18:46:55 -0300

    Adicionado o schema de help

---
+++ lib/kiron_acs/helps/schemas/help.ex
@@ -0,0 +1,34 @@
+defmodule KironAcs.Help do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias KironAcs.{Agent, Pregnant}
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  @required_params [
+    :title,
+    :description,
+    :agent_id,
+    :pregnant_id
+  ]
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id, :agent, :pregnant]}
+
+  schema "helps" do
+    field :description, :string
+    field :title, :string
+
+    belongs_to :agent, Agent
+    belongs_to :pregnant, Pregnant
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+  end
+end
