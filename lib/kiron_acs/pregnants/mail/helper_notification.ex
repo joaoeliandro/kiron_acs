fdd56ca9411c7ea6b4ab0823a9a33b0cd64196ca
João Eliandro Germano <eliandrogermano@gmail.com>
2021-06-29 18:47:42 -0300

    Adicionado salvamento da solicitação de ajuda antes do envio de email

--- lib/kiron_acs/pregnants/mail/helper_notification.ex
+++ lib/kiron_acs/pregnants/mail/helper_notification.ex
@@ -1,11 +1,28 @@
 defmodule KironAcs.Pregnants.Mail.HelperNotification do
   alias KironAcs.Mailer
+
   alias KironAcs.Pregnants.Get, as: PregnantGet
   alias KironAcs.Pregnants.Mail.HelperMail
+
   alias KironAcs.{Agent, Pregnant}
 
+  alias KironAcs.Helps.Create, as: HelpCreate
+
   def send(%{"id" => id, "title" => title, "description" => description}) do
-    {:ok, %Pregnant{full_name: name, email: email, agent: %Agent{} = agent}} = PregnantGet.get_by_id_preload(id)
+    {:ok, %Pregnant{full_name: name, email: email, agent: %Agent{} = agent}} =
+      PregnantGet.get_by_id_preload(id)
+
+    task =
+      Task.async(fn ->
+        HelpCreate.call(%{
+          pregnant_id: id,
+          agent_id: agent.id,
+          title: title,
+          description: description
+        })
+      end)
+
+    Task.await(task)
 
     send_email(name, agent.email, email, title, description)
 
